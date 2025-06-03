6659014570417e4140cd3f111b747866f6717e75
Federico Bosio <federicobosio0209@gmail.com>
2025-06-03 22:33:28 +0200

    .

--- Nazionale 2021/FrittoMisto.py
+++ Nazionale 2021/FrittoMisto.py
@@ -3,8 +3,8 @@ import requests
 url = 'http://frittomisto.challs.olicyber.it/api/register'
 
 r=requests.post(url, json={
-    "username": "wfep0wjofddddddd",
-    "password": "uowefh284ddddd",
+    "username": "sfreifeghgirehe", #valori a caso
+    "password": "wriughigrighrg",
     "invite": "\x00\x01\x02\x03\x04\x05\x06\x07\x08\x09"
 })
 
