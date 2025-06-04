12b8f3bbc512480db3905def835d4597ad969941
Federico Bosio <federicobosio0209@gmail.com>
2025-06-04 19:04:16 +0200

    added challs

---
+++ HighSchools CTF Workshop - 2023 Bari - ITASEC/Xorlandia.py
@@ -0,0 +1,13 @@
+testo_hex = "2e35233a272114063e1a06103d0d3804131c030e1c38150d36013d0202000c1a3d01301f0e1036003d0a16140305113d1b083e03481f"
+chiave_ascii = "gabibbo"
+
+testo_bytes = bytes.fromhex(testo_hex)
+chiave_bytes = chiave_ascii.encode()
+
+risultato_bytes = bytes([testo_bytes[i] ^ chiave_bytes[i % len(chiave_bytes)] for i in range(len(testo_bytes))])
+
+risultato_ascii = risultato_bytes.decode(errors="replace")
+risultato_hex = risultato_bytes.hex()
+
+print("Risultato ASCII:", risultato_ascii)
+print("Risultato Hex:", risultato_hex)
