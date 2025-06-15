634bc5cb34a34fd817263abb2dfa0f3dd73c38ff
Federico Bosio <federicobosio0209@gmail.com>
2025-06-15 21:57:34 +0200

    Update spider.py
    .

--- HighSchools CTF Workshop - 2023 Udine/spider.py
+++ HighSchools CTF Workshop - 2023 Udine/spider.py
@@ -1,33 +1,18 @@
-from bs4 import BeautifulSoup
 import requests
 
-url = 'http://spider.challs.olicyber.it/'
-visited = set()
+url1 = 'http://spider.challs.olicyber.it/supe3s3cretf0lder/flag1.txt'
+url2 = 'http://spider.challs.olicyber.it/standardNonSecretFolder/flag2.txt'
 
+def fetch_flag_part(url):
+        response = requests.get(url, timeout=5)
+        return response.text.strip()
 
-def find_flag(url):
-    queue = [url]
-    while queue:
-        current_url = queue.pop(0)
-        if current_url in visited:
-            continue
-        visited.add(current_url)
+part1 = fetch_flag_part(url1) 
+part2 = fetch_flag_part(url2) 
 
-        response = requests.get(current_url)
-        print(f"richiesta a {current_url}, stato: {response.status_code}")
-        soup = BeautifulSoup(response.text, 'html.parser')
-
-        h1_tag = soup.find('h1')
-        if h1_tag and 'flag' in h1_tag.text.lower():
-            print(f"flag trovata in {current_url}: {h1_tag.text.strip()}")
-            return
-
-        for a_tag in soup.find_all('a', href=True):
-            link = a_tag['href']
-            if link.startswith('/'):
-                link = f'http://spider.challs.olicyber.it{link}'
-            if link not in visited:
-                queue.append(link)
-
-
-find_flag(url)
+if part1 and part2:
+    part1_cleaned = part1.split('\n')[0] 
+    full_flag = part1_cleaned + part2 
+    print(f"flag: {full_flag}")
+else:
+    print("error")
