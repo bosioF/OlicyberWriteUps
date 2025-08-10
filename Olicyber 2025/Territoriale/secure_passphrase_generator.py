86cff7a52705c05cac39ec22f48ead3265bcd407
Federico Bosio <federicobosio0209@gmail.com>
2025-08-10 14:45:42 +0200

    added challs, fixes

--- Olicyber 2025/Territoriale/secure_passphrase_generator.py
+++ Olicyber 2025/Territoriale/secure_passphrase_generator.py
@@ -1,17 +1,19 @@
-from Crypto.Cipher import AES
-from Crypto.Util.Padding import unpad
+from pwn import *
 
-enc_token = "784f623981f52701fe566742e81c042cd6ce5dc4e26a56c95b9ed2a30c0fe7c4f2161023d2115c73f47191e4af7fa841de94c44781b37f09eff0325aa613a203b642e75824b15fe1a44aaa499c0fbdf2"
+HOST = "spg.challs.olicyber.it"
+PORT = int(38052)
 
-enc_token_bytes = bytes.fromhex(enc_token)
+payload = b"gabibbo;index0=0;index1=1;index2=2;index3=3;a=donpollo\x01"
 
-iv = enc_token_bytes[:16]
-ciphertext = enc_token_bytes[16:]
+r = remote(HOST, PORT)
 
-key = b"supersecretkey123"
-
-cipher = AES.new(key, AES.MODE_CBC, iv)
-decrypted = unpad(cipher.decrypt(ciphertext), 16).decode('utf-8')
-
-
-print(f"Token decifrato: {decrypted}")
+r.sendlineafter(b"> ", b"1")
+r.sendlineafter(b"? ", b"gabibbo")
+r.sendlineafter(b"> ", b"1")
+r.sendlineafter(b"? ", payload)
+r.recvline()
+token = r.recvline().decode().split(": ")[1][:5*32]
+r.sendlineafter(b"> ", b"2")
+r.sendlineafter(b"? ", token.encode())
+flag = r.recvline().decode().split()[-1].replace("-", "")
+print(flag)
