cb4db3974d1cb4194afb8c7d00c9a4d29b6ba1a8
Federico Bosio <federicobosio0209@gmail.com>
2025-11-11 16:34:17 +0100

    added HighSchools_CTF_Workshop_2023_Pisa_Internet_ Festival challenges

--- Intro-Olicyber/software/TO_FIX_18.py
+++ Intro-Olicyber/software/TO_FIX_18.py
@@ -1,19 +1,39 @@
-#da finire ancora, non ho tempo rn :(
+# da fixare appena tengo tempo
 
 from pwn import *
 
-r = remote("software-18.challs.olicyber.it", 13001)
+connection = remote("software-18.challs.olicyber.it", 13001)
 
-r.recvuntil(b'iniziare ...')
-r.sendline(b'star this repo')
-x = r.recvuntil(b'-bit')
-if b'64' in x:
-    value = re.findall(r'\b0x([0-9A-Fa-f]{1,8})\b', x.decode())
-    if value:
-        print(value[0])
-else:
-    value = re.findall(r'\b0x([0-9A-Fa-f]{1,8})\b', x.decode())
-    if value:
-        print(value[0])
+def prendiUltimi():
+    for i in range(100):
+        r = connection.recvline().decode()[27:-1]
+        # print("step: ", r)
+        # r = r
+        # print("dati: ", r)
 
-    
+        r = r.split(' packed a ')
+        # print("lista: ", r)
+
+        # byte = r[0]
+        # tipoPacked = r[1]
+
+        # packed = b''
+
+        if r[1] == "64-bit":
+            packed = p64(int(r[0], base=16))
+            # print("64bit", packed)
+        else:
+            packed = p32(int(r[0], base=16))
+            # print("32bit", packed)
+
+        r = connection.recvuntil(b'[+] Result : ')
+        #print("result: ", r.decode())
+        connection.send(packed)
+
+
+connection.recv()
+connection.sendline(b'e')
+
+prendiUltimi()
+
+connection.close()
